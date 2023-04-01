1c059f84bfe6e2852d5403d79d3407e19f339bbb
Pernick <k.kosina@outlook.com>
2023-04-01 11:46:34 +0100

    feat: verify receipt in wasm fn

--- wasm-verifier/src/lib.rs
+++ wasm-verifier/src/lib.rs
@@ -4,14 +4,15 @@
 //     sha::{Digest, DIGEST_WORDS},
 //     Receipt,
 // };
+use methods::WORDLE_ID;
+use risc0_zkvm::Receipt;
 use wasm_bindgen::prelude::*;
 
 #[no_mangle]
 #[wasm_bindgen]
-pub fn is_even(n: i32) -> bool {
-    // TODO: use a real receipt and image_id
-    // let receipt = Receipt::new(&[], &[]);
-    // let image_id = Digest::from([0; DIGEST_WORDS]);
-    // receipt.verify(&image_id).unwrap();
-    n % 2 == 0
+pub fn verify_receipt(receipt_str: String) -> bool {
+    let as_bytes = base64::decode(receipt_str).unwrap();
+    let receipt = bincode::deserialize::<Receipt>(&as_bytes).unwrap();
+
+    receipt.verify(&WORDLE_ID).is_ok()
 }
