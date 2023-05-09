2c8277cab4510d6b8264c68ff975eee6cccebf72
Robert Brada <bert.blader@gmail.com>
2023-05-09 14:36:42 +0200

    feat: pin input

--- frontend/src/components/Row.tsx
+++ frontend/src/components/Row.tsx
@@ -1,9 +1,16 @@
-import { SimpleGrid, TextInput } from "@mantine/core";
+import { PinInput, Stack } from "@mantine/core";
 import { useFormContext } from "../hooks/useWordleForm";
 import useStyles from "./Row.styles";
-
 export interface RowProps {
   disabled: boolean;
+}
+
+function getCharAtIndex(str: string, index: number) {
+  try {
+    return str.charAt(index);
+  } catch (e) {
+    return "";
+  }
 }
 
 export function Row({ disabled }: RowProps) {
@@ -11,19 +18,34 @@ export function Row({ disabled }: RowProps) {
   const form = useFormContext();
 
   return (
-    <SimpleGrid cols={5} spacing="md">
-      {["first", "second", "third", "fourth", "fifth"].map((order, i) => (
-        <TextInput
-          key={order}
-          autoFocus={i === 0}
-          required
-          disabled={disabled}
-          minLength={1}
-          maxLength={1}
-          classNames={{ input: classes.input }}
-          {...form.getInputProps(order)}
-        />
-      ))}
-    </SimpleGrid>
+    <Stack>
+      <PinInput
+        length={5}
+        spacing="md"
+        autoFocus
+        required
+        disabled={disabled}
+        classNames={{ input: classes.input }}
+        placeholder=""
+        inputMode="text"
+        type={RegExp("[a-zA-Z]")}
+        value={
+          form.values.first +
+          form.values.second +
+          form.values.third +
+          form.values.fourth +
+          form.values.fifth
+        }
+        onChange={(input) =>
+          form.setValues({
+            first: getCharAtIndex(input, 0),
+            second: getCharAtIndex(input, 1),
+            third: getCharAtIndex(input, 2),
+            fourth: getCharAtIndex(input, 3),
+            fifth: getCharAtIndex(input, 4),
+          })
+        }
+      />
+    </Stack>
   );
 }
